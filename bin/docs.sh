682635f4095f5e0996f52a446890bb1011c335ca
aaplmath <aaplmath@users.noreply.github.com>
2018-09-23 21:38:53 -0400

    Delete undocumented.json file when generating docs

--- bin/docs.sh
+++ bin/docs.sh
@@ -1,6 +1,6 @@
 dir=${PWD##*/}
 if [ "$dir" = "CaffeineKit" -a -d "Sources" -a -f "logo.png" ]; then
-    jazzy && echo 'section > section > p > img { margin-top: 3em; }' >> docs/css/jazzy.css && rm -r build
+    jazzy && echo 'section > section > p > img { margin-top: 3em; }' >> docs/css/jazzy.css && rm -r build && rm docs/undocumented.json
 else 
     echo "Incorrect directory—must be run from project root"
     exit 1
